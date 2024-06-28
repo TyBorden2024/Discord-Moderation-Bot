76cb8f36985d64a79642e994cad4fe0c441f42f0
Ty Borden 2024 <126364333+127POLECAT324@users.noreply.github.com>
2024-06-27 20:24:45 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+
+var dbd = require("dbd.js")
+var fs = require("fs")
+
+const bot = new dbd.Bot({
+token: "TOKEN",
+prefix: "$getServerVar[prefix]"
+})
+ 
+bot.status({
+  text: "YOUR BOT BIO",
+  type: "PLAYING",
+  time: 12
+})
+ 
+bot.onMessage()
+
+ // Variable
+bot.variables({
+ prefix: "YOUR BOT PREFIX",
+ warn: "0"
+})
+
+ //commands handler
+var reader = fs.readdirSync("./commands/").filter (file => file.endsWith(".js"))
+for(const file of reader) {
+  const command = require(`./commands/${file}`)
+  bot.command({
+name: command.name, 
+code: command.code
+  })
+}
